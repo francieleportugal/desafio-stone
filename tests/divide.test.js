47a86773ad6e613dc4868704adc9f16f0b8c2a0c
Franciele Portugal <franciele.portugal@gmail.com>
2021-02-04 07:36:33 -0300

    replaces "toBe" with "toStrictEqual" to deep equality and fix expected result of the last test

--- tests/divide.test.js
+++ tests/divide.test.js
@@ -2,12 +2,12 @@ const divide = require('../src/divide');
 
 describe('divide', () => {
     it('divide 100 by 3, returns array by person with different values', () => {
-        expect(divide(100, 3)).toBe([33, 33, 34]);
+        expect(divide(100, 3)).toStrictEqual([33, 33, 34]);
     });
     it('divide 100 by 7, returns array by person with different values', () => {
-        expect(divide(100, 7)).toBe([15, 16, 14, 14, 14, 14, 14]);
+        expect(divide(100, 7)).toStrictEqual([14, 14, 14, 14, 14, 15, 15]);
     });
     it('divide 100 by 13, returns array by person with different values', () => {
-        expect(divide(100, 13)).toBe([8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7]);
+        expect(divide(100, 13)).toStrictEqual([7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8]);
     });
 });
