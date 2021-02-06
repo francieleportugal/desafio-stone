8ccca6f6695824b2b80bb903b6d93b2c3e619e6f
Franciele Portugal <franciele.portugal@gmail.com>
2021-02-06 10:34:07 -0300

    comments the functions with jsdoc

--- src/core.js
+++ src/core.js
@@ -1,11 +1,25 @@
+/**
+ * Multiplica o preço de cada item pela sua quantidade e soma todos os valores.
+ * @param {Object[]} shoppingList - Lista de compras
+ * @param {string} shoppingList.name - Nome do item
+ * @param {int} shoppingList.price - Preço do item em centavos
+ * @param {int} shoppingList.amount - Quantidade comprada   
+ * @return {int} Retorna o valor em centavos.  
+ */
 const extractedAccumulatedValue = (shoppingList) => {
     const reducer = (accumulator, currentValue) => {
         return accumulator + (currentValue.amount * currentValue.price);
     };
-
+    
     return shoppingList.reduce(reducer, 0);
 };
 
+/**
+ * Divide o valor entre a quantidade de pessoas de forma igual.
+ * @param {int} total - Valor em centavos
+ * @param {int} amountPeople - Quantidade de pessoas
+ * @return {int[]} Retorna um array onde cada elemento é o valor em centavos que cada pessoa deve pagar.
+ */
 const divideMoneyEqually = (total, amountPeople) => {
     const individualValue = Math.trunc(total / amountPeople);
     const resultByPerson = Array(amountPeople).fill(individualValue);
@@ -20,12 +34,25 @@ const divideMoneyEqually = (total, amountPeople) => {
     return resultByPerson;
 };
 
+/**
+ * Formata a saída dos dados.
+ * @param {String[]} emailList - Lista de e-mails
+ * @param {int[]} resultByPerson - Lista com o valor que cada pessoa deve pagar
+ * @return {Map} Retorna um mapa, onde a chave é o e-mail e valor será quanto a pessoa deve pagar.
+ */
 const formatData = (emailList, resultByPerson) => {
     return new Map(emailList.map((email, index) => (
         [email, resultByPerson[index]]
     )));
 };
 
+
+/**
+ * Interface entra a camada de entrada e a camada core, que abstrai a lógica de calcular o valor em centavos que cada pessoa deve pagar.
+ * @param {String[]} emailList - Lista de e-mails
+ * @param {int[]} resultByPerson - Lista com o valor que cada pessoa deve pagar
+ * @return {Map} Retorna um mapa, onde a chave é o e-mail e valor será quanto a pessoa deve pagar.
+ */
 const coreInterface = (shoppingList, emailList) => {
     if (!shoppingList.length || !emailList.length) {
         throw new Error('Lists should not be empty');
