18d788eb161a8ce330a15113cf0ac9bfb8a060e0
Franciele Portugal <franciele.portugal@gmail.com>
2021-02-07 15:48:07 -0300

    Revert "moves validations to interface layer"
    This reverts commit 1be861d75b46256f794af2b4f7f53ce13a378d74.

--- src/interface.js
+++ src/interface.js
@@ -47,38 +47,6 @@ const formatData = (emailList, resultByPerson) => {
 };
 
 /**
- * Valida se há e-mails duplicados na lista.
- * @param {String[]} emailList - Lista de e-mails
- * @return {Boolean} Indica se há e-mails duplicados.
- */
-const hasDuplicates = (emailList) => {
-    return new Set(emailList).size !== emailList.length; 
-};
-
-/**
- * Valida dados de entrada.
- * @param {Object[]} shoppingList - Lista de compras
- * @param {string} shoppingList.name - Nome do item
- * @param {int} shoppingList.price - Preço do item em centavos
- * @param {int} shoppingList.amount - Quantidade comprada
- * @param {String[]} emailList - Lista de e-mails
- * @return {void}
- * @throws Será lançado um erro se qualquer lista estiver vazia.
- * @throws Será lançado um erro se houver e-mails duplicados.
- */
-const validateInputData = (shoppingList, emailList) => {
-    // Valida se as listas de entrada não estão vazias. Caso estejam é emitido um erro
-    if (!shoppingList.length || !emailList.length) {
-        throw new Error('Lists should not be empty');
-    }
-
-    // Valida se há e-mails duplicados
-    if (hasDuplicates(emailList)) {
-        throw new Error('There are duplicate emails');
-    }
-};
-
-/**
  * Lê os arquivos CSV para obter os dados de entrada e executa a lógica do core.
  * Responsável pela comunicação entre o main e o core.  
  * @return {Promise<Map>} Retorna uma promise com o mapa dos resultados 
@@ -88,8 +56,7 @@ const executeWithDataInputByCsvFile = async () => {
     const shoppingList = await getShoppingList();
     const emailList = await getEmailList();
 
-    validateInputData(shoppingList, emailList);
-    const result = coreInterface(shoppingList, emailList.length);
+    const result = coreInterface(shoppingList, emailList);
 
     return formatData(emailList, result);
 };
@@ -106,8 +73,7 @@ const executeWithDataInputByCsvFile = async () => {
  * (valor que cada pessoa deve pagar).
  */
 const execute = (shoppingList, emailList) => {
-    validateInputData(shoppingList, emailList);
-    const result = coreInterface(shoppingList, emailList.length);
+    const result = coreInterface(shoppingList, emailList);
 
     return formatData(emailList, result);
 };
--- tests/coreInterface.test.js
+++ tests/coreInterface.test.js
@@ -15,11 +15,47 @@ describe('coreInterface', () => {
                 amount: 3,
             },
         ];
+        const emailList = [
+            'user1@gmail.com',
+            'user2@gmail.com',
+            'user3@gmail.com',
+            'user4@gmail.com',
+            'user5@gmail.com',
+            'user6@gmail.com',
+            'user7@gmail.com',
+            'user8@gmail.com',
+            'user9@gmail.com',
+        ];
 
-        expect(coreInterface(shoppingList, 9))
+        expect(coreInterface(shoppingList, emailList))
             .toStrictEqual([
                 862794, 862794, 862794, 862794,
                 862794, 862794, 862793, 862793, 862793,
             ]);
+    });
+    it('send empty shopping list and emails list, returns values', () => {
+        const shoppingList = [];
+        const emailList = ['user1@gmail.com', 'user2@gmail.com'];
+
+        expect(() => coreInterface(shoppingList, emailList))
+            .toThrow('Lists should not be empty');
+    });
+    it('send empty shopping list and emails list, returns values', () => {        
+        const shoppingList = [
+            {
+                name: 'item 1',
+                price: 1079430,
+                amount: 3,
+            },
+            {
+                name: 'item 2',
+                price: 1508951,
+                amount: 3,
+            },
+        ];
+        const emailList = ['user1@gmail.com', 'user1@gmail.com'];
+
+        expect(() => coreInterface(shoppingList, emailList))
+            .toThrow('There are duplicate emails');
     });
 });
